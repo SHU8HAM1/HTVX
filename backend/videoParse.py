4d0a5825932497ff568c911b98da98554780b828
athanie2003 <112116242+athanie2003@users.noreply.github.com>
2025-10-04 02:27:12 -0400

    Push backend files

---
+++ backend/videoParse.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# get video link from frontend 
+@app.route('/api/video', methods=['POST'])
+def get_video_data():
+    if request.is_json:
+        # data in JSON format
+        data = request.json
+        video = data.get('video') # video property in body has video
+
+        # store video in cloud storage
+        # connect storage bucket to Snowflake
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
